34e313c31f999a21a84e661199e399131152cc78
Seth Baah <baahs@Seths-MacBook-Pro-2.local>
2025-01-13 14:15:46 +0100

    NEW: Add GitHub Actions for CI pipeline

---
+++ karma.conf.js
@@ -0,0 +1,31 @@
+module.exports = function (config) {
+  const isCI = process.env.CI === 'true';
+
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      clearContext: false,
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/movielist'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    singleRun: isCI,
+    browsers: isCI ? ['ChromeHeadless'] : ['Chrome'], // Use ChromeHeadless in CI, Chrome locally
+    restartOnFileChange: true,
+  });
+};
