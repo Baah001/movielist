b33da5bb0e0fa621f01f6851ea72802af23506f9
Seth Baah <baahs@Seths-MacBook-Pro-2.local>
2025-01-15 07:55:12 +0100

    Auto-generated commit

---
+++ chunk-DHTDZFJP.js
@@ -0,0 +1,1 @@
+var e={production:!0,tmdbApiKey:"692bfd47a119e54a79dd7355a3f3c487",tmdbBaseUrl:"https://api.themoviedb.org/3/",assetsBaseUrl:"/movielist"};export{e as a};
--- chunk-PMS2J7UC.js
+++
@@ -1,1 +0,0 @@
-var e={production:!1,tmdbApiKey:"692bfd47a119e54a79dd7355a3f3c487",tmdbBaseUrl:"https://api.themoviedb.org/3/",assetsBaseUrl:"/"};export{e as a};
